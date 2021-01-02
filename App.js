fb278bcd8751fe8295172b7fb4cd4917a6c74232
alestor123 <alestoraldous@gmail.com>
2021-01-02 21:14:25 +0530

    added chalk

--- App.js
+++ App.js
@@ -1,2 +1,6 @@
 var Conf = require('conf'),
-options = require('minimist')(process.argv.slice(2));
+options = require('minimist')(process.argv.slice(2)),
+config = new Conf(),
+twilio = require('twilio'),
+message = options.m || options.message || 'No Message',
+audio = options.a || options.audio;
